2f6ea8d9ffcdbe0ca4026ef228075f8157764112
Nishantjain719 <66901836+Nishantjain719@users.noreply.github.com>
2021-05-25 09:12:56 +0530

    Create 45.java

---
+++ 45.java
@@ -0,0 +1,28 @@
+// S = "ababcbacadefegdehijhklij" output=[9,7,8]
+import java.util.ArrayList;
+import java.util.List;
+
+class Solutin42 {
+    public List<Integer> partitionLabels(String S) {
+        if (S == null || S.length() == 0) return null;
+
+        List<Integer> output_arr = new ArrayList<>();
+
+        int[] last_indices = new int[26];
+        for (int i=0; i<S.length(); i++) {
+            last_indices[S.charAt(i)-'a'] = i;
+        }
+        int start = 0;
+        int end = 0;
+        for (int i=0; i<S.length(); i++) {
+            end = Math.max(end, last_indices[S.charAt(i)-'a']);
+            if (i == end) {
+                output_arr.add(end-start+1);
+                start = end + 1;
+            }
+        }
+
+        return output_arr;
+        
+    }
+}
